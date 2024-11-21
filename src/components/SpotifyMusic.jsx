5d67e930703866b2ac54e17d7c624e0af3ba8223
YpCIIIaK <bigboyvova01@gmail.com>
2024-11-21 17:30:06 +0700

    22

--- src/components/SpotifyMusic.jsx
+++ src/components/SpotifyMusic.jsx
@@ -1,32 +1,33 @@
 const SpotifyMusic = () => {
     return (
         <div className="mt-6">
-            <h4 className="text-lg font-bold">One`s of the Favorite Tracks</h4>
-            <iframe
-                src="https://open.spotify.com/embed/track/1T8Wf6c2AWWYNdc7tFAhzX?utm_source=generator"
-                width="100%"
-                height="80"
-                frameBorder="0"
-                allow="encrypted-media"
-                className="mt-4 rounded-lg shadow-lg"
-            ></iframe>
-            <iframe
-                src="https://open.spotify.com/embed/track/3NkCuoxxx5VZ6T6xTsmjFD?utm_source=generator"
-                width="100%"
-                height="80"
-                frameBorder="0"
-                allow="encrypted-media"
-                className="mt-4 rounded-lg shadow-lg"
-            ></iframe>
-            <iframe
-                src="https://open.spotify.com/embed/track/1StpXCfxWblt9D1I4hDVIx?utm_source=generator"
-                width="100%"
-                height="80"
-                frameBorder="0"
-                allow="encrypted-media"
-                className="mt-4 rounded-lg shadow-lg"
-            ></iframe>
-
+            <h4 className="text-lg font-bold mb-4">One of the Favorite Tracks</h4>
+            <div className="flex flex-wrap gap-4">
+                <iframe
+                    src="https://open.spotify.com/embed/track/1T8Wf6c2AWWYNdc7tFAhzX?utm_source=generator"
+                    width="400"
+                    height="100"
+                    frameBorder="0"
+                    allow="encrypted-media"
+                    className="rounded-lg shadow-lg"
+                ></iframe>
+                <iframe
+                    src="https://open.spotify.com/embed/track/3NkCuoxxx5VZ6T6xTsmjFD?utm_source=generator"
+                    width="400"
+                    height="100"
+                    frameBorder="0"
+                    allow="encrypted-media"
+                    className="rounded-lg shadow-lg"
+                ></iframe>
+                <iframe
+                    src="https://open.spotify.com/embed/track/1StpXCfxWblt9D1I4hDVIx?utm_source=generator"
+                    width="400"
+                    height="100"
+                    frameBorder="0"
+                    allow="encrypted-media"
+                    className="rounded-lg shadow-lg"
+                ></iframe>
+            </div>
         </div>
     );
 };
